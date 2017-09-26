4ef2f97b0a6201da47d7d7996459e34ac06a0f6a
Joshua M. Doe <oss@nvl.army.mil>
2017-09-26 14:10:22 -0400

    matroxsrc: modify FindMatrox.cmake to find 32-bit lib

--- cmake/modules/FindMatrox.cmake
+++ cmake/modules/FindMatrox.cmake
@@ -12,7 +12,23 @@
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
 if (NOT MATROX_DIR)
-    set (MATROX_DIR "C:/Program Files/Matrox Imaging/Mil" CACHE PATH "Directory containing Matrox MIL SDK includes and libraries")
+    # 32-bit dir on win32
+    file(TO_CMAKE_PATH "$ENV{ProgramFiles}" _PROG_FILES)
+    # 32-bit dir on win64
+    file(TO_CMAKE_PATH "$ENV{ProgramFiles(x86)}" _PROG_FILES_X86)
+    # 64-bit dir on win64
+    file(TO_CMAKE_PATH "$ENV{ProgramW6432}" _PROG_FILES_W6432)
+
+    if (CMAKE_SIZEOF_VOID_P MATCHES "8")
+        set(_PROGFILESDIR "${_PROG_FILES_W6432}")
+    else ()
+        if (_PROG_FILES_X86)
+            set(_PROGFILESDIR "${_PROG_FILES_X86}")
+        else ()
+            set(_PROGFILESDIR "${_PROG_FILES}")
+        endif ()
+    endif ()
+    set (MATROX_DIR "${_PROGFILESDIR}/Matrox Imaging/Mil" CACHE PATH "Directory containing Matrox MIL SDK includes and libraries")
 endif ()
 
 find_path (MATROX_INCLUDE_DIR Mil.h
